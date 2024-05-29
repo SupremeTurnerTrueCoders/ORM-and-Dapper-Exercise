0cf5900ecf5341589d6a8c2eb4f184f985f900c6
SupremeTurnerTrueCoders <email@example.com>
2024-05-29 15:58:50 -0400

    Completed ORM DAPPER

---
+++ ORM-Dapper/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+
+        void CreateProduct(string name, double price, int categoryID);
+
+        public void UpdateProduct(Products products);
+        public void DeleteProduct(int id);
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection connection = new MySqlConnection(connString);
+
+           var repo = new DapperProductRepository(connection);
+
+            repo.CreateProduct("newStuff", 20, 1);
+
+            var productToUpdate = repo.GetProduct(974); 
+
+           var products = repo.GetAllProducts();
+
+            productToUpdate.Name = "UPDATED!!";
+            productToUpdate.Price = 12.99;
+            productToUpdate.CategoryID = 1;
+            productToUpdate.OnSale = false;
+            productToUpdate.StockLevel = 1000;
+
+            productRepository.UpdateProduct(productToUpdate);
+
+            productRepository.Deleteproduct(974);
+
+            var products = productRepository.GetAllProducts();
+
+
+            foreach ( var prod in products )
+            {
+                Console.WriteLine($"{prod.ProductID} {prod.Name}");
+            }
         }
     }
 }
